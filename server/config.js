88c5a1ce786b423ce37f4cfe20306fe0c16ffb92
xlakostax <konstantin.veselovskii@gmail.com>
2019-12-04 22:17:48 +0200

    small fixes

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 const config = {
   mailer: {
     host: 'smtp.ethereal.email',
-    port: 587,
+    port: 5870,
     secure: false, // true for 465, false for other ports
     service: "ethereal",
     tls:{
