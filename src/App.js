13b9b7ee79822fb4e8b62dd802ec334a745688b4
xlakostax <konstantin.veselovskii@gmail.com>
2019-12-05 10:36:41 +0200

    small fixes

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
     const message = this.state.message;
     axios({
       method: 'POST',
-      url:'./send',
+      url:'localhost/send',
       data: {
         name: name,
         email: email,
