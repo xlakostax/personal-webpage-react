03401b31393b48e30c3d1d8158e7a413258ae845
xlakostax <konstantin.veselovskii@gmail.com>
2020-01-20 00:44:20 +0200

    minor fixes and about was transformed (hooks are added)

--- client/src/routes/About.js
+++ client/src/routes/About.js
@@ -5,6 +5,8 @@ import BurgerMenu from '../components/BurgerMenu';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+import { useState, useEffect } from 'react';
+
 const Main = styled.main`
   grid-area: main;
   position: relative;
@@ -39,55 +41,33 @@ const Main = styled.main`
   }
 `;
 
-export default class About extends Component {
-  constructor() {
-    super();
-    this.state = {
-      width: window.innerWidth
-    };
-  }
+const About = () => {
+  var currentWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var visible = (currentWidth < 1024) ? {display: 'block'} : {display: 'none'}
+  const [style, setStyle] = useState(visible);
+  useEffect( () => {
+    setStyle(visible);
+    window.addEventListener('resize', setStyle);
 
-  componentDidMount = () => {
-    window.addEventListener( 'resize', this.handleWindowSizeChange );
-  }
-
-/* make sure to remove the listener when the component is not mounted anymore */
-  componentWillUnmount = () => {
-    window.removeEventListener( 'resize', this.handleWindowSizeChange );
-  }
-
-  handleWindowSizeChange = () => {
-    this.setState({ width: window.innerWidth });
-  };
-
-  render() {
-    const isMobile = window.innerWidth <= 1024;
-    if (isMobile) {
-      return(
-        <>
-          <BurgerMenu />
-          <Main>
-            <article>
-              <h1>About</h1>
-              <p>My name is Konstantin Veselovskii. I was born and grew up in Petrozavodsk, a small town in Karelia. I am passionate my job. I have been studying the web development since 2019. I focus on writing clean, elegant and efficient code.</p>
-              <p>If you need some inspiring ideas, <Link to = '/contacts'>let’s connect</Link> to get the process started.</p>
-            </article>
-          </Main>
-          <Footer />
-        </>
-      )
-    } else {
-      return(
-        <>
-          <Header />
-          <Main>
-            <h1>About</h1>
-            <p>My name is Konstantin Veselovskii. I was born and grew up in Petrozavodsk, a small town in Karelia. I am passionate my job. I have been studying the web development since 2019. I focus on writing clean, elegant and efficient code.</p><br />
-            <p>If you need some inspiring ideas, <Link to = '/contacts'>let’s connect</Link> to get the process started.</p>
-          </Main>
-          <Footer />
-        </>
-      );
+    /* remove resize listener */
+    return () => {
+      window.removeEventListener('resize', setStyle);
     }
-  }
+  }, []);
+  return(
+    <>
+      <span style = { visible } >
+        <BurgerMenu/>
+      </span>
+      <Main>
+        <article>
+          <h1>About</h1>
+          <p>My name is Konstantin Veselovskii. I was born and grew up in Petrozavodsk, a small town in Karelia. I am passionate my job. I have been studying the web development since 2019. I focus on writing clean, elegant and efficient code.</p>
+          <p>If you need some inspiring ideas, <Link to = '/contacts'>let’s connect</Link> to get the process started.</p>
+        </article>
+      </Main>
+      <Footer />
+    </>
+  )
 }
+export default About;
