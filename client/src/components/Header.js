e61ea88980acf78404f04ec0a75b3fb4d1bebcd6
xlakostax <konstantin.veselovskii@gmail.com>
2020-01-26 15:48:29 +0200

    fixes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -18,9 +18,10 @@ const HeaderTag = styled.header`
   box-shadow: 0 1px rgba( 220, 220, 220, 1 );
   /* to avoid 'jumping' of fixed header */
   transform:translateZ(0);
-  -webkit-transform:translateZ(0);
-  -moz-transform:translateZ(0);
-  -o-transform:translateZ(0);
+    -webkit-transform:translateZ(0);
+    -moz-transform:translateZ(0);
+    -ms-transform: translateZ(0);
+    -o-transform:translateZ(0);
   /* & .active {
     color: rgb(255, 99, 71);
   }
@@ -44,17 +45,40 @@ const HeaderTag = styled.header`
     font-weight: 400;
     font-size: 1.2em;
     & span {
+      display: inline-block
+      transition: transform .2s ease-in-out;
+      transform: translateY(0);
+        -webkit-transform: translateY(0);
+        -moz-transform: translateY(0);
+        -ms-transition: translateY(0);
+        -o-transform: translateY(0);
+    }
+    & :nth-child(2) {
       font-weight: 800;
+    }
+    &:hover :nth-child(2) {
+      transform: translateY(-2px);
+        -webkit-transform: translateY(-2px);
+        -moz-transform: translateY(-2px);
+        -ms-transition: translateY(-2px);
+        -o-transform: translateY(-2px);
+    }
+    &:hover :first-child {
+      transform: translateY(2px);
+        -webkit-transform: translateY(2px);
+        -moz-transform: translateY(2px);
+        -ms-transition: translateY(2px);
+        -o-transform: translateY(2px);
     }
   }
   & nav {
     @media ( max-width: 1024px ) {
       color: rgb( 255, 255, 255 );
       transition: transform 1s ease-in-out;
-      -webkit-transition: transform 1s ease-in-out;
-      -moz-transition: transform 1s ease-in-out;
-      -ms-transition: transform 1s ease-in-out;
-      -o-transition: transform 1s ease-in-out;
+        -webkit-transition: transform 1s ease-in-out;
+        -moz-transition: transform 1s ease-in-out;
+        -ms-transition: transform 1s ease-in-out;
+        -o-transition: transform 1s ease-in-out;
       top: 0;
       bottom: 0;
       min-height: 100vh;
@@ -62,6 +86,10 @@ const HeaderTag = styled.header`
       width: 100vw;
       background-color: rgba( 47, 47, 47, 0.9 );
       transform: translateX( -200vw );
+        -webkit-transform: translateX(-200vw);
+        -moz-transform: translateX(-200vw);
+        -ms-transition: translateX(-200vw);
+        -o-transform: translateX(-200vw);
     }
     & ul {
       display: flex;
@@ -95,6 +123,10 @@ const HeaderTag = styled.header`
           font-size: 1.2em;
           background-image: linear-gradient( transparent 0%, transparent calc(50% - 9px), rgba(255, 99, 71, 0.35) calc(50% - 9px), rgba(255, 99, 71, 0.35) 100% );
           transition: background-position 120ms ease-in-out;
+            -webkit-transition: background-position 120ms ease-in-out;
+            -moz-transition: background-position 120ms ease-in-out;
+            -ms-transition: background-position 120ms ease-in-out;
+            -o-transition: background-position 120ms ease-in-out;
           background-size: 100% 180%;
           @media (max-width: 1024px) {
             background-image: none;
@@ -113,22 +145,36 @@ const HeaderTag = styled.header`
   & .menu-btn {
     display: none;
     &:checked ~ nav {
-      transition: transform 1s ease-in-out;
       transform: translateX(-10%);
+        -webkit-transform: translateX(-10%);
+        -moz-transform: translateX(-10%);
+        -ms-transition: translateX(-10%);
+        -o-transform: translateX(-10%);
     }
     &:checked ~ .menu-icon .navicon {
       background: transparent;
       &:before {
         transform: rotate(-45deg);
-        top: 0;
-        transition: background 0.9s ease-in-out;
-        background: rgb( 255, 255, 255 );
+          -webkit-transform: rotate(-45deg);
+          -moz-transform: rotate(-45deg);
+          -ms-transition: rotate(-45deg);
+          -o-transform: rotate(-45deg);
       }
       &:after {
         transform: rotate(45deg);
+          -webkit-transform: rotate(45deg);
+          -moz-transform: rotate(45deg);
+          -ms-transition: rotate(45deg);
+          -o-transform: rotate(45deg);
+      }
+      &:before, :after {
         top: 0;
-        transition: background 0.9s ease-in-out;
         background: rgb( 255, 255, 255 );
+        transition: top .3s ease-out, transform .3s ease-out .3s, background .3s ease-out .7s;
+          -webkit-transition: top .3s ease-out, transform .3s ease-out .3s, background .3s ease-out .7s;
+          -moz-transition: top .3s ease-out, transform .3s ease-out .3s, background .3s ease-out .7s;
+          -ms-transition: top .3s ease-out, transform .3s ease-out .3s, background .3s ease-out .7s;
+          -o-transition: top .3s ease-out, transform .3s ease-out .3s, background .3s ease-out .7s;
       }
     }
   }
@@ -141,7 +187,11 @@ const HeaderTag = styled.header`
       display: none;
       height: 2px;
       position: relative;
-      transition: background .2s ease-out;
+      transition: background .1s ease-out 0.3s;
+        -webkit-transition: background .1s ease-out 0.3s;
+        -moz-transition: background .1s ease-out 0.3s;
+        -ms-transition: background .1s ease-out 0.3s;
+        -o-transition: background .1s ease-out 0.3s;
       width: 2em;
       @media (max-width: 1024px){
         display: block;
@@ -152,18 +202,26 @@ const HeaderTag = styled.header`
         display: block;
         height: 100%;
         position: absolute;
-        transition: all .3s ease-out;
+        transition: transform .3s ease-out, top .3s ease-out .3s;
+          -webkit-transition: transform .3s ease-out, top .3s ease-out .3s;
+          -moz-transition: transform .3s ease-out, top .3s ease-out .3s;
+          -ms-transition: transform .3s ease-out, top .3s ease-out .3s;
+          -o-transition: transform .3s ease-out, top .3s ease-out .3s;
         width: 100%;
-        margin-bottom: .3em;
+        transform: rotate(0);
+          -webkit-transform: rotate(0);
+          -moz-transform: rotate(0);
+          -ms-transform: rotate(0);
+          -o-transform: rotate(0);
         @media (max-width: 1024px){
 
         }
       }
       &:before {
-        top: 5px;
+        top: 10px;
       }
       &:after {
-        top: -5px;
+        top: -10px;
       }
     }
   }
@@ -183,7 +241,7 @@ const Header = () => {
   return(
     <HeaderTag>
       <NavLink exact to = '/'>
-        <h2 className = 'logo'>Konstantin<span>Veselovskii</span></h2>
+        <h2 className = 'logo'><span>Konstantin</span><span>Veselovskii</span></h2>
       </NavLink>
         <input id = 'menu-btn' className = 'menu-btn' type = 'checkbox'/>
         <label className = 'menu-icon' htmlFor = 'menu-btn'>
